89f130a94f6207f47d07f974ee2631724fd1d294
COGMLS <mlsmatheus@hotmail.com>
2021-10-04 20:03:05 -0300

    Adição de projeto JAVA do Eclipse e 1º teste de funcionalidade.

---
+++ Stock_Control/src/stockControlPackage/MainClass.java
@@ -0,0 +1,11 @@
+package stockControlPackage;
+
+public class MainClass
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Teste de funcionamento de projeto Java no Eclipse.");
+	}
+}
