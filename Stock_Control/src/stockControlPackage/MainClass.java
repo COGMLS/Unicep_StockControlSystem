e48ca0cd1c2510d46838c13c53299830be4c9dfa
COGMLS <mlsmatheus@hotmail.com>
2021-10-22 12:50:49 -0300

    Criada a sequência de funcionamento em Main. Adicionados comentários às sequências.

--- Stock_Control/src/stockControlPackage/MainClass.java
+++ Stock_Control/src/stockControlPackage/MainClass.java
@@ -6,5 +6,24 @@ public class MainClass
 	{
 		// TODO Auto-generated method stub
 		System.out.println("Teste de funcionamento de projeto Java no Eclipse.");
+
+		// Instancia a lista de produtos que estará disponível como DB.
+
+		if(args.size > 0 && args[0] == "-SimulationMode")
+		{
+			// Instancia a uma lista de usuários.
+
+			// Loop para o modo de simulação:
+
+		}
+		else
+		{
+			// Loop de funcionamento em modo de interação:
+			String userEntry;
+			do
+			{
+
+			}while(userEntry != "Sair")
+		}
 	}
 }
