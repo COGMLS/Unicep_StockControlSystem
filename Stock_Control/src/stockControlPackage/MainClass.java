20859c97ab030236c1bee89c6ffb83470fc7ed76
COGMLS <mlsmatheus@hotmail.com>
2021-11-02 17:19:02 -0300

    Adicionada apresentação dos autores.

--- Stock_Control/src/stockControlPackage/MainClass.java
+++ Stock_Control/src/stockControlPackage/MainClass.java
@@ -4,12 +4,14 @@ public class MainClass
 {
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
-		System.out.println("Teste de funcionamento de projeto Java no Eclipse.");
+		// Apresentação dos responsáveis:
+		System.out.println("Unicep - Sistema de Controle de Estoque (Stock Control System)");
+		System.out.println("\n\nAlunos:\n\tBruno Alves de Araújo | RA: 4200870\n\tMatheus Lopes Silvati | RA: 4200872");
+		System.out.println("Professor: Wesley Pecoraro\nCurso: Eng. de Computação\nDisciplina: Programação Orientada ao Objeto");
 
 		// Instancia a lista de produtos que estará disponível como DB.
 
-		if(args.size > 0 && args[0] == "-SimulationMode")
+		if(args.length > 0 && args[0] == "-SimulationMode")
 		{
 			// Instancia a uma lista de usuários.
 
@@ -23,7 +25,7 @@ public class MainClass
 			do
 			{
 
-			}while(userEntry != "Sair")
+			}while(userEntry != "Sair");
 		}
 	}
 }
