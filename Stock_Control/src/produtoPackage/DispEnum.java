e6bd61602c0b66ad42ce30444ad7031ae7158b88
COGMLS <mlsmatheus@hotmail.com>
2021-11-03 21:59:44 -0300

    Adicionados comentários aos valores do enumerador

--- Stock_Control/src/produtoPackage/DispEnum.java
+++ Stock_Control/src/produtoPackage/DispEnum.java
@@ -2,10 +2,10 @@ package produtoPackage;
 
 public enum DispEnum
 {
-    INDISPONIVEL,   // Sem Estoque.
-    MUITOBAIXA,
-    BAIXA,
-    MEDIA,
-    ALTA,
-    MUITOALTA   // Estoque cheio.
+    INDISPONIVEL,   // Sem Estoque. Quantidade = 0
+    MUITOBAIXA,     // Estoque <= 10%
+    BAIXA,          // 25% >= Estoque > 10%
+    MEDIA,          // 50 >= Estoque > 25%
+    ALTA,           // 75% >= Estoque > 50%
+    MUITOALTA       // Estoque cheio ou > 75%
 }
