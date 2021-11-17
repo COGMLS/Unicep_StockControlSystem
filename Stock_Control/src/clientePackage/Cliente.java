5847f66d5034544063b30f460e91a6186bb67610
COGMLS <mlsmatheus@hotmail.com>
2021-11-17 19:05:28 -0300

    Adicionados novos comentários. Adicionada a função PesquisarProduto Corrigido o tratamento de entrada de usuário Realizadas alterações na sequência lógica de algumas funções.

--- Stock_Control/src/clientePackage/Cliente.java
+++ Stock_Control/src/clientePackage/Cliente.java
@@ -4,6 +4,14 @@ import java.util.Scanner;
 
 import estoquePackage.Estoque;
 import reservaPackage.Reserva;
+import comprasPackage.FinalizarCompra;
+
+/** Classe de controle do cliente.
+ * ------------------------------------
+ * Cuida das operações de interação do 
+ * cliente com o sistema de reserva do
+ * controle de estoque.
+*/
 
 public class Cliente
 {
@@ -25,22 +33,46 @@ public class Cliente
         System.out.println("ID do Cliente: " + this.ID_Cliente);
     }
 
+    //Pesquisa por um produto e suas informações.
+    public void PesquisarProduto(Estoque estoque)
+    {
+        System.out.print("Entre com a ID de um produto: ");
+        Scanner ID_Prod = new Scanner(System.in);
+
+        String ID_ProdStr = ID_Prod.next();
+
+        try
+        {
+            int ID_ProdInt = Integer.parseInt(ID_ProdStr);
+
+            estoque.encontrarID(ID_ProdInt);
+        }
+        catch (Exception e)
+        {
+            System.out.println("\n\n" + e.getMessage() +"\n\n");
+        }
+    }
+
     //Reserva e adiciona os produtos do estoque para a lista de compras.
     public void ReservarProdutoCliente(Estoque estoque)
     {
         System.out.print("Entre com o código do produto para reservar: ");
         Scanner ID_Prod = new Scanner(System.in);
 
+        String ID_ProdStr = ID_Prod.next();
+
         try
         {
-            int ID_ProdInt = Integer.parseInt(ID_Prod.toString());
+            int ID_ProdInt = Integer.parseInt(ID_ProdStr);
 
-            System.out.println("Entre com a quantidade de produtos a ser retirada: ");
+            System.out.print("Entre com a quantidade de produtos a ser retirada: ");
             Scanner RetirarQuant = new Scanner(System.in);
+
+            String RetirarQuantStr = RetirarQuant.next();
 
             try
             {
-                int RetirarQuantInt = Integer.parseInt(RetirarQuant.toString());
+                int RetirarQuantInt = Integer.parseInt(RetirarQuantStr);
 
                 ClienteCompras.RetirarProdutosDoEstoque(estoque, ID_ProdInt, RetirarQuantInt);
             }
@@ -61,16 +93,20 @@ public class Cliente
         System.out.print("Entre com o código do produto para devolver ao estoque: ");
         Scanner ID_Prod = new Scanner(System.in);
 
+        String ID_ProdStr = ID_Prod.next();
+
         try
         {
-            int ID_ProdInt = Integer.parseInt(ID_Prod.toString());
+            int ID_ProdInt = Integer.parseInt(ID_ProdStr);
 
-            System.out.println("Entre com a quantidade de produtos a ser devolvida ao estoque: ");
+            System.out.print("Entre com a quantidade de produtos a ser devolvida ao estoque: ");
             Scanner RetirarQuant = new Scanner(System.in);
+
+            String RetirarQuantStr = RetirarQuant.next();
 
             try
             {
-                int RetirarQuantInt = Integer.parseInt(RetirarQuant.toString());
+                int RetirarQuantInt = Integer.parseInt(RetirarQuantStr);
 
                 ClienteCompras.DevolverProdutosAoEstoque(estoque, ID_ProdInt, RetirarQuantInt);
             }
@@ -92,14 +128,14 @@ public class Cliente
     }
 
     //Finalizar a compra.
-    public void FinalizarCompra()
+    public void FinalizarCompra(Estoque estoque)
     {
         boolean ConfirmarCompra;
 
-        System.out.println("Deseja finalizar a compra? (s|n)");
+        System.out.print("Deseja finalizar a compra ou cancelar (c)? (s|n|c)  ");
         Scanner UserConfirm = new Scanner(System.in);
 
-        String UserConfirmStr = UserConfirm.toString();
+        String UserConfirmStr = UserConfirm.next();
 
         switch (UserConfirmStr)
         {
@@ -111,11 +147,26 @@ public class Cliente
             case "N":
                 ConfirmarCompra = false;
                 break;
+            case "c":
+            case "C":
+                ConfirmarCompra = false;
+                ClienteCompras.FinalizarCompra(estoque, ConfirmarCompra);
+                break;
             default:
                 ConfirmarCompra = false;
                 break;
         }
 
-        ClienteCompras.FinalizarCompra(ConfirmarCompra);
+        if(ConfirmarCompra)
+        {
+            //Entra em contato com o banco.
+            Boolean CompraAutorizada = FinalizarCompra.ConcluirCompra();
+
+            //Define o status da compra.
+            ClienteCompras.setCompraAutorizada(CompraAutorizada);
+
+            //Contata a reserva do cliente para finalizar a compra.
+            ClienteCompras.FinalizarCompra(estoque, ConfirmarCompra);
+        }
     }
 }
