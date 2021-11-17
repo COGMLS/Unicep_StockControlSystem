95595582e9b5557089faa7a5c0c97fcb2075e622
COGMLS <mlsmatheus@hotmail.com>
2021-11-17 19:16:33 -0300

    Adicionadas novas dependências ao código. Retiradas variáreis locais da classe. Alteradas as funções para static. Finalizadas as funções da classe.

--- Stock_Control/src/reposicaoPackage/Reposicao.java
+++ Stock_Control/src/reposicaoPackage/Reposicao.java
@@ -1,5 +1,9 @@
 package reposicaoPackage;
 
+import java.util.Random;
+
+import produtoPackage.Produto;
+
 /**Classe Reposicao
  *-----------------------------------
  *Gerencia a reposição de um determinado
@@ -9,52 +13,76 @@ package reposicaoPackage;
 
 public class Reposicao
 {
-    private float PreçoAntigo;
-    private float PrecoNovo;
-    private float MargemDeLucro;
-    private float PrecoDeCompra;
-    private int QuantProdutosNovos;
-
     //Contata o forncedor para a requisição de novos produtos.
-    private void ContatarFornecedor()
+    private static int ContatarFornecedorQuant(int QuantR)
     {
-        //Se houver o produto disponível, seguir para a compra.
-        if()
-        {
-            //Se o forncedor tiver a quantidade necessária de produtos, os adquire na quantidade desejada.
-            if()
-            {
-                ComprarNovosProdutos(N);
-            }
-            else    //Caso o forncedor não tenha a quantidade de produtos desejada, adiquirir a quntidade que houver.
-            {
-                ComprarNovosProdutos(n);
-            }
-        }
-    };
+        Random QuantFornecedor = new Random();
+        Random QuantMaxMod = new Random();
+
+        int QuantMaxModInt = QuantMaxMod.nextInt(100 + 1) + 1;
+
+        //Retorna um valor entre 0 e a quantidade máxima que pode ser 1x até 100x a quantidade recomendada forncecida.
+        return QuantFornecedor.nextInt((QuantR * QuantMaxModInt) + 0) + 0;
+    }
+
+    //Fonece o valor dos produtos que serão repostos. NOTA: função apenas para testes.
+    private static float ContatarFornecedorPreco(float PrecoAntigo)
+    {
+        return PrecoAntigo;
+    }
 
     //Calcula o novo preço para o produto que será vendido, através da média ponderada.
-    private float CalcularNovoPreco()
+    private static float CalcularNovoPreco(Produto produtoEstoque, float PrecoCompra, int QuantComprada, float MargemDeLucro)
     {
         float NovoPreco = 0.0f;
 
-        NovoPreco = (this.PreçoAntigo * this.Quantidade + this.PrecoDeCompra * this.QuantProdutosNovos)/(this.QuantProdutosNovos + this.Quantidade)
+        //Calcula a margem de lucro no produto.
+        PrecoCompra *= MargemDeLucro;
 
-        this.PrecoNovo = NovoPreco;
-    };
+        //Calcula o novo preço do produto que será vendido.
+        NovoPreco = (produtoEstoque.getPreco() * produtoEstoque.getQuantidade() + PrecoCompra * QuantComprada) / (produtoEstoque.getQuantidade() + QuantComprada);
+
+        return NovoPreco;
+    }
 
     //Contata o forncedor confirmando a aquisição dos produtos.
-    private void ComprarNovosProdutos(int Quant)
+    private static int ComprarNovosProdutos(int Quant)
     {
-
-    };
+        return Quant;
+    }
 
     //Repoem ao estoque os produtos adiquiridos.
-    private void ReporEstoque(int Quant)
+    public static void ReporEstoque(Produto produto, int EncomendarQuant)
     {
-        ContatarFornecedor();
+        //Contata o forncedor e verifica a quantidade máxima (0 a EncomendarQuant) disponível pelo forncedor.
+        int QuantFornecida = ContatarFornecedorQuant(produto.getQuantidadeRecomendada());
 
-        this.Quantidade += this.QuantProdutosNovos;
-        this.Preco = this.PrecoNovo;
-    };
+        if(QuantFornecida > 0)
+        {
+            int QuantConfirmada = 0;
+
+            //Caso a quantidade máxima do forncedor seja menor, comprar o máximo possível.
+            if(QuantFornecida < EncomendarQuant)
+            {
+                QuantConfirmada = QuantFornecida;
+            }
+            else
+            {
+                QuantConfirmada = EncomendarQuant;
+            }
+
+            //Confirma a compra dos produtos para reposição do estoque.
+            int QuantComprada = ComprarNovosProdutos(QuantConfirmada);
+
+            //Contata o forncedor para receber o preço dos novos produtos.
+            float PrecoNovosProdutos = ContatarFornecedorPreco(produto.getPreco());
+
+            //Calcula o preço final para a venda dos produtos. Margem de lucro definida para 20%.
+            float NovoPreco = CalcularNovoPreco(produto, PrecoNovosProdutos, QuantComprada, 0.2f);
+
+            //Define o novo preço unitário do produto e sua quantidade no esetoque.
+            produto.setPreco(NovoPreco);
+            produto.setQuantidade(QuantComprada);
+        }
+    }
 }
